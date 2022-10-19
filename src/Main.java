5b6f62b6a46763020bf632d3ec0a76267aa7d948
luan_nguyen_3@sfu.ca <57047641+luan30092000@users.noreply.github.com>
2022-10-19 14:48:06 -0700

    Init Tower of Hanoi simulation

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,8 @@ public class Main {
 
 
     public static void main(String[] args) {
-        int[] arr1 = new int[1];
-        int[] arr2 = new int[3];
-        arr1[0] = 1;
-
-        arr2[0] = 2;
-        arr2[1] = 3;
-        arr2[2] = 4;
-
-
-        System.out.println(LeetCodeArray.findMedianSortedArrays(arr1, arr2));
+        TowerOfHanoi newGame = new TowerOfHanoi(5);
+        newGame.printGame();
 
     }
 }
---
+++ src/TowerOfHanoi.java
@@ -0,0 +1,35 @@
+
+public class TowerOfHanoi {
+    public int[][] column;
+    private int size_col_1 = 0, size_col_2 = 0, size_col_3 = 0;
+    private int general_capacity;
+
+    public TowerOfHanoi(int size) {
+        column = new int[3][size];
+        general_capacity = size;
+        initialFirstColumn(size);
+    }
+
+    private void initialFirstColumn(int size) {
+        for (int i = 0; i < size; i++) {
+            column[0][i] = size - i;
+            size_col_1++;
+        }
+    }
+
+    public void moveDish(int[] from, int[] to) {
+        if (from.)
+    }
+
+    public void printGame() {
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Col " + i + ": ");
+            for (int j = 0; j < column[i].length; j++) {
+                System.out.print(column[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+}
